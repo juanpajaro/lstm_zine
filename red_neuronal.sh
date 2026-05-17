a37a322d3d8a9b2946a8cfc87ac42fd964f40384
juan <juanpajaro@javeriana.edu.co>
2026-05-17 14:45:37 -0500

    modificando cpus

--- red_neuronal.sh
+++ red_neuronal.sh
@@ -4,7 +4,7 @@
 #SBATCH -o logs/resultado_%j.out      ## Output file
 #SBATCH -e logs/error_%j.err
 #SBATCH --ntasks=1             ## Number of tasks (analyses) to run
-#SBATCH --cpus-per-task=100      ## The number of threads the code will use
+#SBATCH --cpus-per-task=32      ## The number of threads the code will use
 #SBATCH --mem-per-cpu=100M     ## Real memory(MB) per CPU required by the job.
 
 ## Load the python interpreter
